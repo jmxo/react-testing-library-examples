d1ea45c5a625810fcff52cbf5e10ac15d693c941
Mohamed Ismat <mohamedismat91@gmail.com>
2019-05-22 11:13:14 +0200

    Mock http requests with jest.mock

---
+++ src/__tests__/http-jest-mock.js
@@ -0,0 +1,26 @@
+import 'jest-dom/extend-expect'
+import 'react-testing-library/cleanup-after-each'
+
+import React from 'react'
+import {render, fireEvent, wait} from 'react-testing-library'
+import {GreetingLoader} from '../greeting-loader-01-mocking'
+import {loadGreeting as mockLoadGreeting} from '../api'
+
+jest.mock('../api', () => {
+  return {
+    loadGreeting: jest.fn(subject =>
+      Promise.resolve({data: {greeting: `Hi ${subject}`}}),
+    ),
+  }
+})
+
+test('loads greetings on click', async () => {
+  const {getByLabelText, getByText, getByTestId} = render(<GreetingLoader />)
+  const nameInput = getByLabelText(/name/i)
+  const loadButton = getByText(/load/i)
+  nameInput.value = 'Mary'
+  fireEvent.click(loadButton)
+  expect(mockLoadGreeting).toHaveBeenCalledTimes(1)
+  expect(mockLoadGreeting).toHaveBeenCalledWith('Mary')
+  await wait(() => expect(getByTestId('greeting')).toHaveTextContent(`Hi Mary`))
+})
