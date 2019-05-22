9ff741010ca43f7912fbd17a7c30bc6e3e417219
Mohamed Ismat <mohamedismat91@gmail.com>
2019-05-22 10:46:52 +0200

    Text accessibility with jest-axe

---
+++ src/__tests__/a11y.js
@@ -0,0 +1,21 @@
+import 'jest-dom/extend-expect'
+import 'react-testing-library/cleanup-after-each'
+import 'jest-axe/extend-expect'
+import React from 'react'
+import {render} from 'react-testing-library'
+import {axe} from 'jest-axe'
+
+function Form() {
+  return (
+    <form>
+      <label htmlFor="username">Username</label>
+      <input id="username" placeholder="username" name="username" />
+    </form>
+  )
+}
+
+test('the form is accessible', async () => {
+  const {container} = render(<Form />)
+  const results = await axe(container.innerHTML)
+  expect(results).toHaveNoViolations()
+})
