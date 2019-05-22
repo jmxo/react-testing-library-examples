5881268fa26e621f9d5297401ed6a29c61634a8c
Mohamed Ismat <mohamedismat91@gmail.com>
2019-05-22 09:57:33 +0200

    Use react-testing-library to render and test comps

---
+++ src/__tests__/react-testing-library.js
@@ -0,0 +1,10 @@
+import 'jest-dom/extend-expect'
+import React from 'react'
+import {render} from 'react-testing-library'
+import {FavoriteNumber} from '../favorite-number'
+
+test('renders a number input with a label "Favorite Number"', () => {
+  const {getByLabelText} = render(<FavoriteNumber />)
+  const input = getByLabelText(/favorite number/i)
+  expect(input).toHaveAttribute('type', 'number')
+})
