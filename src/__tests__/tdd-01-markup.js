2bd088a2ab592b15cb42cb00f6783388206db10d
Mohamed Ismat <mohamedismat91@gmail.com>
2019-05-23 12:20:07 +0200

    Create form using tdd

---
+++ src/__tests__/tdd-01-markup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {render} from 'react-testing-library'
+import {Editor} from '../post-editor-01-markup'
+
+test('renders a form with title, content, tags, and a submit button', () => {
+  const {getByLabelText, getByText} = render(<Editor />)
+  getByLabelText(/title/i)
+  getByLabelText(/content/i)
+  getByLabelText(/tags/i)
+  getByText(/submit/i)
+})
