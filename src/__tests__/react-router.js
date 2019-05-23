19e0b197479baacb7dbedfbb767ff7eb8c7eecf4
Mohamed Ismat <mohamedismat91@gmail.com>
2019-05-23 16:01:24 +0200

    Test comps that use react-router w/ memory history

---
+++ src/__tests__/react-router.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Router} from 'react-router-dom'
+import {createMemoryHistory} from 'history'
+import {render, fireEvent} from 'react-testing-library'
+import {Main} from '../main'
+
+test('Main renders home and about and I can navigate to those pages', () => {
+  const history = createMemoryHistory({initialEntries: ['/']})
+
+  const {getByText, getByTestId, queryByTestId} = render(
+    <Router history={history}>
+      <Main />
+    </Router>,
+  )
+  expect(getByTestId('home-screen')).toBeInTheDocument()
+  expect(queryByTestId('about-screen')).not.toBeInTheDocument()
+  fireEvent.click(getByText(/about/i))
+  expect(queryByTestId('home-screen')).not.toBeInTheDocument()
+  expect(getByTestId('about-screen')).toBeInTheDocument()
+})
