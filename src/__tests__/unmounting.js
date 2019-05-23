14d168edd1ec85f468052ca6dfc0174f90141d85
Mohamed Ismat <mohamedismat91@gmail.com>
2019-05-23 18:13:38 +0200

    Test unmounting  and avoiding memory leaks

---
+++ src/__tests__/unmounting.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {render} from 'react-testing-library'
+import {Countdown} from '../countdown'
+
+jest.useFakeTimers()
+
+beforeEach(() => {
+  jest.spyOn(console, 'error').mockImplementation(() => {})
+})
+
+afterEach(() => {
+  console.error.mockRestore()
+})
+
+test('does not attempt to set state when unmounted (to prevent memory leaks', () => {
+  const {unmount} = render(<Countdown />)
+  unmount()
+  jest.runOnlyPendingTimers()
+  expect(console.error).not.toHaveBeenCalled()
+})
