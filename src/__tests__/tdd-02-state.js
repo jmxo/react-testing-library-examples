1fd876227f41386e6a67b12db23410101f122dfe
Mohamed Ismat <mohamedismat91@gmail.com>
2019-05-23 12:36:21 +0200

    Test drive form submission, test submit handler

---
+++ src/__tests__/tdd-02-state.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {render, fireEvent} from 'react-testing-library'
+import {Editor} from '../post-editor-02-state'
+
+test('renders a form with title, content, tags, and a submit button', () => {
+  const {getByLabelText, getByText} = render(<Editor />)
+  getByLabelText(/title/i)
+  getByLabelText(/content/i)
+  getByLabelText(/tags/i)
+  const submitButton = getByText(/submit/i)
+
+  fireEvent.click(submitButton)
+  expect(submitButton).toBeDisabled()
+})
