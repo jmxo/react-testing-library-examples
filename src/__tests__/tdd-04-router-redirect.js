ebdd9e899a3d3d195f92f08f602909e7feb77d52
Mohamed Ismat <mohamedismat91@gmail.com>
2019-05-23 13:15:38 +0200

    Mock react-router Redirect with tdd

---
+++ src/__tests__/tdd-04-router-redirect.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import {render, fireEvent, wait} from 'react-testing-library'
+import {Redirect as MockRedirect} from 'react-router'
+import {savePost as mockSavePost} from '../api'
+import {Editor} from '../post-editor-04-router-redirect'
+
+jest.mock('react-router', () => {
+  return {
+    Redirect: jest.fn(() => null),
+  }
+})
+
+jest.mock('../api', () => {
+  return {
+    savePost: jest.fn(() => Promise.resolve()),
+  }
+})
+
+afterEach(() => {
+  mockSavePost.mockClear()
+  MockRedirect.mockClear()
+})
+
+test('renders a form with title, content, tags, and a submit button', async () => {
+  const fakeUser = {id: 'user-1'}
+  const {getByLabelText, getByText} = render(<Editor user={fakeUser} />)
+  const fakePost = {
+    title: 'Test Title',
+    content: 'Test content',
+    tags: ['tag1', 'tag2'],
+  }
+
+  getByLabelText(/title/i).value = fakePost.title
+  getByLabelText(/content/i).value = fakePost.content
+  getByLabelText(/tags/i).value = fakePost.tags.join(', ')
+  const submitButton = getByText(/submit/i)
+
+  fireEvent.click(submitButton)
+  expect(submitButton).toBeDisabled()
+  expect(mockSavePost).toHaveBeenCalledTimes(1)
+  expect(mockSavePost).toHaveBeenCalledWith({
+    ...fakePost,
+    authorId: fakeUser.id,
+  })
+
+  // wait for this assertion to pass. (max 4.5 seconds)
+  await wait(() => expect(MockRedirect).toHaveBeenCalledTimes(1))
+  expect(MockRedirect).toHaveBeenCalledWith({to: '/'}, {})
+})
