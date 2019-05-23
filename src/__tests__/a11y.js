56a5ca9f0d78df48b22e853ec4d91759ff0910d5
Mohamed Ismat <mohamedismat91@gmail.com>
2019-05-23 18:22:34 +0200

    Fix breaking test from jest-axe 3.1.1

--- src/__tests__/a11y.js
+++ src/__tests__/a11y.js
@@ -13,7 +13,15 @@ function Form() {
 }
 
 test('the form is accessible', async () => {
-  const {container} = render(<Form />)
+  /* 
+  react-testing-library creates a container and adds it to document.body,
+  Then jest-axe creates another container and also appends it to document.body here.
+  So we end up with two rendered versions of <MyComponent /> in document.body which is why axe-core is throwing the duplicate ids error
+  Workaround: Pass an element to the container option of react-testing-library's render() so that it doesn't add it to the document at all.
+  */
+
+  const div = document.createElement('div')
+  const {container} = render(<Form />, {container: div})
   const results = await axe(container.innerHTML)
   expect(results).toHaveNoViolations()
 })
